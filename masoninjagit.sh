f82f632c7ea17dc809bf52af86c43248954daf1d
mka <=>
2019-02-04 08:47:02 -0800

    AA

---
+++ masoninjagit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  Masonininja;
+git config --global user.name  Masoninja;
+git config --global user.user Masoninja;
+git config --global user.email masonkerryallen@yahoo.com;
+git config --list;
